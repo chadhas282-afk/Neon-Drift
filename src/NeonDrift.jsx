bbf681569ddf1cc638c63afaedadbc1129ce2c34
chadhas282-afk <chadhas282@gmail.com>
2026-05-05 10:33:37 +0530

    Refactor rendering function to improve shape drawing in NeonDrift component

--- src/NeonDrift.jsx
+++ src/NeonDrift.jsx
@@ -29,3 +29,6 @@ const C = {
 function rr(ctx, x, y, w, h, r) {
   ctx.beginPath();
   ctx.moveTo(x + r, y); ctx.lineTo(x + w - r, y);
+    ctx.quadraticCurveTo(x + w, y, x + w, y + r);
+  ctx.lineTo(x + w, y + h - r);
+  ctx.quadraticCurveTo(x + w, y + h, x + w - r, y + h);
