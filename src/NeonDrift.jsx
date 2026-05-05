fa41c03a6b443f65e637efc1620abb16913f7f0d
chadhas282-afk <chadhas282@gmail.com>
2026-05-05 10:33:51 +0530

    Fix rendering function to complete shape drawing in NeonDrift component

--- src/NeonDrift.jsx
+++ src/NeonDrift.jsx
@@ -32,3 +32,6 @@ function rr(ctx, x, y, w, h, r) {
     ctx.quadraticCurveTo(x + w, y, x + w, y + r);
   ctx.lineTo(x + w, y + h - r);
   ctx.quadraticCurveTo(x + w, y + h, x + w - r, y + h);
+    ctx.lineTo(x + r, y + h);
+  ctx.quadraticCurveTo(x, y + h, x, y + h - r);
+  ctx.lineTo(x, y + r);
