28b0002337d72ffdeac32dc1ffb15d13df32c2ad
chadhas282-afk <chadhas282@gmail.com>
2026-05-05 10:30:09 +0530

    Initialize main application structure with React and render NeonDrift component

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import NeonDrift from './NeonDrift'
+
+createRoot(document.getElementById('root')).render(
+    <NeonDrift />
+)
